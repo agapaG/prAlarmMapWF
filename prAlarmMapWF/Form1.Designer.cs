30542944ba57aee2fa000ae7c243c960c826e248
aworker <koniev_25@ukr.net>
2022-08-13 20:07:27 +0300

    Добавьте файлы проекта.

---
+++ prAlarmMapWF/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace prAlarmMapWF
+{
+    partial class Map
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AlarmMap = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // AlarmMap
+            // 
+            this.AlarmMap.Bearing = 0F;
+            this.AlarmMap.CanDragMap = true;
+            this.AlarmMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.AlarmMap.GrayScaleMode = false;
+            this.AlarmMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.AlarmMap.LevelsKeepInMemmory = 5;
+            this.AlarmMap.Location = new System.Drawing.Point(-1, 0);
+            this.AlarmMap.MarkersEnabled = true;
+            this.AlarmMap.MaxZoom = 2;
+            this.AlarmMap.MinZoom = 2;
+            this.AlarmMap.MouseWheelZoomEnabled = true;
+            this.AlarmMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.AlarmMap.Name = "AlarmMap";
+            this.AlarmMap.NegativeMode = false;
+            this.AlarmMap.PolygonsEnabled = true;
+            this.AlarmMap.RetryLoadTile = 0;
+            this.AlarmMap.RoutesEnabled = true;
+            this.AlarmMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.AlarmMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.AlarmMap.ShowTileGridLines = false;
+            this.AlarmMap.Size = new System.Drawing.Size(809, 459);
+            this.AlarmMap.TabIndex = 0;
+            this.AlarmMap.Zoom = 0D;
+            this.AlarmMap.Load += new System.EventHandler(this.AlarmMap_Load);
+            // 
+            // Map
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AlarmMap);
+            this.Name = "Map";
+            this.Text = "Map";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Map_Load);
+            this.Resize += new System.EventHandler(this.Map_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl AlarmMap;
+    }
+}
+
---
+++ prAlarmMapWF/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+
+
+namespace prAlarmMapWF
+{
+    public partial class Map : Form
+    {
+        GMarkerGoogle markerGoogle = null;
+        //Создам список маркеров
+        GMapOverlay markersOverlay = new GMapOverlay("markers");
+
+        public Map()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.BackColor = Color.LightGray;
+            
+        }
+
+        private void AlarmMap_Load(object sender, EventArgs e)
+        {
+            //Настройки для компонента GMap
+            AlarmMap.Bearing = 0; //Горизонтальное положение карты
+            //Возможность перетаскивать карту левой кнопкой мыши
+            AlarmMap.CanDragMap = true;
+            AlarmMap.DragButton = MouseButtons.Left;
+
+            AlarmMap.GrayScaleMode = true;
+
+            //Отображение всех маркеров
+            AlarmMap.MarkersEnabled = true;
+
+            //Максимальное/Минимальное приближения
+            AlarmMap.MaxZoom = 20;
+            AlarmMap.MinZoom = 2;
+
+            //Курсор мыши в центр карты
+            AlarmMap.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
+
+            //Отключение негативного режима
+            AlarmMap.NegativeMode = false;
+
+            //Разрешение полигонов
+            AlarmMap.PolygonsEnabled = true;
+            //Разрешение маршрутов
+            AlarmMap.RoutesEnabled = true;
+
+            //Скрытие внешней сетки карты
+            AlarmMap.ShowTileGridLines = false;
+
+            //При загрузке 13ти кратное увеличение
+            AlarmMap.Zoom = 13.135;
+
+            //Убрать красный крестик по центру
+            AlarmMap.ShowCenter = false;
+
+            //Чья карта используется
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            AlarmMap.MapProvider = GMapProviders.CzechMap;
+            //Необходимо подколючение к интернету
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+
+            //Стартовый центр карты
+            AlarmMap.Position = new PointLatLng(49.989897385959935, 36.22941235773933);
+
+
+            //Инициализация маркера и его координат
+            //PointLatLng dd = AlarmMap.F("Україна, Харків, Академіка Проскури вулиця, 10А");
+
+            markerGoogle = new GMarkerGoogle(new PointLatLng(50.04357, 36.28210), GMarkerGoogleType.red);
+            markerGoogle.ToolTip = new GMapRoundedToolTip(markerGoogle);
+
+            //dd = {{Lat=50,0445987, Lng=36,2824705789611}}
+
+            //Текст обображаемый с маркером
+            markerGoogle.ToolTipText = "Мой дом";
+
+            //Добавляю маркер в список маркеров
+            markersOverlay.Markers.Add(markerGoogle);
+            AlarmMap.Overlays.Add(markersOverlay);
+
+        }
+
+        private void Map_Load(object sender, EventArgs e)
+        {
+            Control ctrl = (Control)sender;
+            AlarmMap.Height = ctrl.Height;
+            AlarmMap.Width = ctrl.Width;
+
+            //GeoCoderStatusCode geoCoderStatusCode = new GeoCoderStatusCode();
+            //PointLatLng? dd = GMapProviders.OpenStreetMap.GetPoint("Україна, Харків, Академіка Проскури вулиця, 10А", out geoCoderStatusCode);
+            
+        }
+
+        private void Map_Resize(object sender, EventArgs e)
+        {
+            Control ctrl = (Control)sender;
+            AlarmMap.Height = ctrl.Height;
+            AlarmMap.Width = ctrl.Width; 
+        }
+    }
+}
